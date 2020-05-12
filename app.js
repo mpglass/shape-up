4d2bc7619be2935d75135a48b8c5c5e11c4d61b7
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-05-12 14:06:28 -0500

    finished lab

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ class Shape {
         this.height = height;
         this.div = $('<div class="shape"></div>');
         this.makeShape();
+        this.triggers();
     }
     calculateArea() {
         return this.width * this.height;
@@ -20,8 +21,7 @@ class Shape {
         return (height + width) * 2
     }
     genPosition(offset) {
-       return Math.floor(Math.random() * (600 - offset));
-        
+        return Math.floor(Math.random() * (600 - offset));
     }
     makeShape() {
         this.div.css({
@@ -32,50 +32,76 @@ class Shape {
         });
         $('#canvas').append(this.div);
     }
-    sidepanelInfo() {
-        $('#sdpnShapeName').val(this.div.attr('id'));
-        $('#sdpnWidth').val(this.width);
-        $('#sdpnHeight').val(this.height);
-    }
     triggers() {
         this.div.click(() => this.sidepanelInfo());
         this.div.dblclick(() => this.bye());
     }
+    sidepanelInfo() {
+        console.log('click', this)
+        $('#sdpnShapeName').val(this.div.attr('id'));
+        $('#sdpnWidth').val(this.width);
+        $('#sdpnHeight').val(this.height);
+
+    }
     bye() {
         this.div.remove();
+        console.log('bye click')
         $('.form-control').val('');
     }
 }
 
 class Square extends Shape {
     constructor(sideLength) {
-        sideLength = $('.sideLength').val();
         super(sideLength, sideLength);
         this.div.attr('id', 'square');
     }
     calculatePerimeter() {
-        const sideLength = this.sideLength
+        const sideLength = this.width
         return sideLength * 4
+    }
+    calculateArea() {
+        const sideLength = this.width
+        return sideLength * sideLength
+    }
+    sidepanelInfo() {
+        super.sidepanelInfo();
+        $('#sdpnPerimeter').val(this.calculatePerimeter());
+        $('#sdpnArea').val(this.calculateArea());
+        $('#sdpnRadius').val('N/A');
     }
 }
 
 class Circle extends Shape {
     constructor(radius) {
         super(2 * radius, 2 * radius);
-    }   
-    calculateArea(radius) {
-        // const radius = this.width / 2;
+        this.radius = radius
+        this.div.attr('id', 'circle');
+    }
+    calculateArea() {
+        const radius = this.width/2;
         return Math.PI * radius * radius;
     }
-    calculatePerimeter(radius) {
-        return 2 * Math.PI * radius
+    calculatePerimeter() {
+        const radius = this.width/2;
+        return 2 * Math.PI * radius;
+    }
+    sidepanelInfo() {
+        super.sidepanelInfo();
+        $('#sdpnPerimeter').val(this.calculatePerimeter());
+        $('#sdpnArea').val(this.calculateArea());
+        $('#sdpnRadius').val(this.radius);
     }
 }
 
 class Triangle extends Shape {
     constructor(height) {
         super(height, height);
-    }    
+        this.div.attr('id', 'triangle')
+        this.div.css({
+            height: 0,
+            width: 0,
+        })
+    }
     calculateArea() {
         const height = this.height
         return (height * height) / 2;
@@ -85,20 +111,49 @@ class Triangle extends Shape {
         const hypot = height * 1.41421356237
         return (height * 2) + hypot;
     }
+    sidepanelInfo() {
+        super.sidepanelInfo();
+        $('#sdpnPerimeter').val(this.calculatePerimeter());
+        $('#sdpnArea').val(this.calculateArea());
+        $('#sdpnRadius').val('N/A');
+    }
 }
 
 class Rectangle extends Shape {
-    constructor(height, width) {
+    constructor(width, height) {
         super(width, height);
+        this.div.attr('id', 'rectangle')
+    }
+    sidepanelInfo() {
+        super.sidepanelInfo();
+        $('#sdpnPerimeter').val(this.calculatePerimeter());
+        $('#sdpnArea').val(this.calculateArea());
+        $('#sdpnRadius').val('N/A');
     }
 }
-new Square(200);
-new Circle(150);
 
-// $('#squareBtn').click(function (event) {
-//     event.preventDefault();
-//     console.log(sideLength);
-//     new Square();
-//         $('#canvas').append(new Square)
-// })
 
+$('#squareBtn').click(function (event) {
+    event.preventDefault();
+    let sideLength = $('.sideLength').val();
+    new Square(sideLength);
+})
+
+$('#circleBtn').click(function (event) {
+    event.preventDefault();
+    let radius = $('.radius').val();
+    new Circle(radius);
+})
+
+$('#triangleBtn').click(function (event) {
+    event.preventDefault();
+    let height = $('.heightTri').val();
+    new Triangle(height);
+})
+
+$('#rectangleBtn').click(function (event) {
+    event.preventDefault();
+    let width = $('.width').val();
+    let height = $('.height').val();
+    new Rectangle(width, height);
+})
